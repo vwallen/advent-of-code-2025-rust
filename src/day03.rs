7ae090ff1b0c00f2f0352e6eaf7496dd81f6530c
Victor W Allen <vwallen@antlab.net>
2025-12-03 10:13:05 -0800

    Slightly less terrible generalized version

--- src/day03.rs
+++ src/day03.rs
@@ -1,128 +1,34 @@
 use crate::read_input_lines;
 use anyhow::Result;
 
-pub fn prepare(file_name: &str) -> Result<Vec<String>> {
-    let input = read_input_lines(file_name);
+pub fn prepare(file_name: &str) -> Result<Vec<Vec<u64>>> {
+    let input = read_input_lines(file_name)
+        .iter()
+        .map(|line| {
+            line
+                .chars()
+                .map(|c| c.to_digit(10).map(u64::from).unwrap())
+                .collect()
+        })
+        .collect();
     Ok(input)
 }
 
-pub fn find_highest(batteries:&String) -> u64 {
-    let batteries: Vec<u64> = batteries
-        .chars()
-        .map(|c| c.to_digit(10).map(u64::from).unwrap())
-        .collect();
-    let mut digits = vec![0u64; 2];
+pub fn find_highest_n(n:usize, batteries:&Vec<u64>) -> u64 {
+    let mut digits = vec![0u64; n];
+    let len = batteries.len();
     for (i, d) in batteries.iter().enumerate() {
-        if d > &digits[0] && i < batteries.len() - 1 {
-            digits[0] = *d;
-            digits[1] = 0;
-        } else if d > &digits[1] {
-            digits[1] = *d;
+        for j in 0..n {
+            if d > &digits[j] && i < len - (n - (j + 1)) {
+                digits[j] = *d;
+                if j + 1 < n {
+                   digits[j + 1] = 0;
+                }
+                break
+            }
         }
     }
-    digits[0] * 10 + digits[1]
-}
 
-
-pub fn find_highest_12(batteries:&String) -> u64 {
-    let batteries: Vec<u64> = batteries
-        .chars()
-        .map(|c| c.to_digit(10).map(u64::from).unwrap())
-        .collect();
-    let mut digits = vec![0u64; 12];
-    for (i, d) in batteries.iter().enumerate() {
-        if d > &digits[0] && i < batteries.len() - 11 {
-            digits[0] = *d;
-            digits[1] = 0;
-            digits[2] = 0;
-            digits[3] = 0;
-            digits[4] = 0;
-            digits[5] = 0;
-            digits[6] = 0;
-            digits[7] = 0;
-            digits[8] = 0;
-            digits[9] = 0;
-            digits[10] = 0;
-            digits[11] = 0;
-        } else if d > &digits[1] && i < batteries.len() - 10 {
-            digits[1] = *d;
-            digits[2] = 0;
-            digits[3] = 0;
-            digits[4] = 0;
-            digits[5] = 0;
-            digits[6] = 0;
-            digits[7] = 0;
-            digits[8] = 0;
-            digits[9] = 0;
-            digits[10] = 0;
-            digits[11] = 0;
-        } else if d > &digits[2] && i < batteries.len() - 9 {
-            digits[2] = *d;
-            digits[3] = 0;
-            digits[4] = 0;
-            digits[5] = 0;
-            digits[6] = 0;
-            digits[7] = 0;
-            digits[8] = 0;
-            digits[9] = 0;
-            digits[10] = 0;
-            digits[11] = 0;
-        } else if d > &digits[3] && i < batteries.len() - 8 {
-            digits[3] = *d;
-            digits[4] = 0;
-            digits[5] = 0;
-            digits[6] = 0;
-            digits[7] = 0;
-            digits[8] = 0;
-            digits[9] = 0;
-            digits[10] = 0;
-            digits[11] = 0;
-        } else if d > &digits[4] && i < batteries.len() - 7 {
-            digits[4] = *d;
-            digits[5] = 0;
-            digits[6] = 0;
-            digits[7] = 0;
-            digits[8] = 0;
-            digits[9] = 0;
-            digits[10] = 0;
-            digits[11] = 0;
-        } else if d > &digits[5] && i < batteries.len() - 6 {
-            digits[5] = *d;
-            digits[6] = 0;
-            digits[7] = 0;
-            digits[8] = 0;
-            digits[9] = 0;
-            digits[10] = 0;
-            digits[11] = 0;
-        } else if d > &digits[6] && i < batteries.len() - 5 {
-            digits[6] = *d;
-            digits[7] = 0;
-            digits[8] = 0;
-            digits[9] = 0;
-            digits[10] = 0;
-            digits[11] = 0;
-        } else if d > &digits[7] && i < batteries.len() - 4 {
-            digits[7] = *d;
-            digits[8] = 0;
-            digits[9] = 0;
-            digits[10] = 0;
-            digits[11] = 0;
-        } else if d > &digits[8] && i < batteries.len() - 3 {
-            digits[8] = *d;
-            digits[9] = 0;
-            digits[10] = 0;
-            digits[11] = 0;
-        } else if d > &digits[9] && i < batteries.len() - 2 {
-            digits[9] = *d;
-            digits[10] = 0;
-            digits[11] = 0;
-        } else if d > &digits[10] && i < batteries.len() - 1 {
-            digits[10] = *d;
-            digits[11] = 0;
-        } else if d > &digits[11] && i < batteries.len() - 0 {
-            digits[11] = *d;
-        }
-    }
     digits
         .iter()
         .rev()
@@ -132,13 +38,13 @@ pub fn find_highest_12(batteries:&String) -> u64 {
 }
 
 
-pub fn part_1(input: &Vec<String>) -> Option<u64> {
-    let total:u64 = input.iter().map(find_highest).sum();
+pub fn part_1(input: &Vec<Vec<u64>>) -> Option<u64> {
+    let total:u64 = input.iter().map(|battery| find_highest_n(2, battery)).sum();
     Some(total)
 }
 
-pub fn part_2(input: &Vec<String>) -> Option<u64> {
-    let total:u64 = input.iter().map(find_highest_12).sum();
+pub fn part_2(input: &Vec<Vec<u64>>) -> Option<u64> {
+    let total:u64 = input.iter().map(|battery| find_highest_n(12, battery)).sum();
     Some(total)
 }
 
@@ -148,12 +54,12 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_find_highest() {
-        assert_eq!(find_highest(&"12345".into()), 45);
-        assert_eq!(find_highest(&"12151".into()), 51);
-        assert_eq!(find_highest(&"35215".into()), 55);
-        assert_eq!(find_highest(&"12453".into()), 53);
-        assert_eq!(find_highest(&"45321".into()), 53);
+    fn test_find_highest_n() {
+        assert_eq!(find_highest_n(2,&vec![1,2,3,4,5]), 45);
+        assert_eq!(find_highest_n(2,&vec![1,2,1,5,1]), 51);
+        assert_eq!(find_highest_n(2, &vec![3,5,2,1,5]), 55);
+        assert_eq!(find_highest_n(2, &vec![1,2,4,5,3]), 53);
+        assert_eq!(find_highest_n(2, &vec![4,5,3,2,1]), 53);
     }
 
     #[test]
