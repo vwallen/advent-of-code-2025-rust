153de13ef713a83849a7fbd0a3cdf05da4e1c082
Victor W Allen <vwallen@antlab.net>
2025-12-04 12:20:55 -0800

    reformat

--- src/day04.rs
+++ src/day04.rs
@@ -1,21 +1,19 @@
-use std::cell::Cell;
-use rustc_hash::FxHashMap;
 use crate::read_input_lines;
 use anyhow::Result;
+use rustc_hash::FxHashMap;
+use std::cell::Cell;
 
-static MAX_NEIGHBORS:u8 = 4;
+static MAX_NEIGHBORS: u8 = 4;
 
 type Warehouse = FxHashMap<(isize, isize), Cell<u8>>;
 
 pub fn prepare(file_name: &str) -> Result<Warehouse> {
-    let input = read_input_lines(file_name);
-    let output = input
+    let output = read_input_lines(file_name)
         .iter()
         .enumerate()
         .map(|(y, line)| {
             let y = y as isize;
-            line
-                .chars()
+            line.chars()
                 .enumerate()
                 .filter_map(|(x, c)| {
                     let x = x as isize;
@@ -28,20 +26,19 @@ pub fn prepare(file_name: &str) -> Result<Warehouse> {
                 .collect::<Vec<(isize, isize, u8)>>()
         })
         .fold(Warehouse::default(), |mut out, row| {
-            row.into_iter()
-                .for_each(|(x, y, space)| {
-                    out.insert((x, y), Cell::new(space));
-                });
+            row.into_iter().for_each(|(x, y, space)| {
+                out.insert((x, y), Cell::new(space));
+            });
             out
         });
     update_neighbors(&output);
     Ok(output)
 }
 
-pub fn update_neighbors(spaces:&Warehouse) {
+pub fn update_neighbors(spaces: &Warehouse) {
     for ((x, y), space) in spaces.into_iter() {
         let (x, y) = (*x, *y);
-        let count:u8 = [
+        let count: u8 = [
             (x - 1, y),
             (x - 1, y - 1),
             (x - 1, y + 1),
@@ -51,28 +48,26 @@ pub fn update_neighbors(spaces:&Warehouse) {
             (x, y + 1),
             (x, y - 1),
         ]
-            .into_iter()
-            .fold(0, |count, (x, y)| {
-                if let Some(_) = spaces.get(&(x, y)) {
-                    count + 1
-                } else {
-                    count
-                }
+        .into_iter()
+        .fold(0, |count, (x, y)| {
+            if let Some(_) = spaces.get(&(x, y)) {
+                count + 1
+            } else {
+                count
+            }
         });
         space.set(count);
-    };
+    }
 }
 
-pub fn remove_rolls(spaces:&mut Warehouse) -> usize {
+pub fn remove_rolls(spaces: &mut Warehouse) -> usize {
     let mut remove = Vec::new();
     for ((x, y), space) in spaces.into_iter() {
         let (x, y) = (*x, *y);
         if space.get() < MAX_NEIGHBORS {
             remove.push((x, y));
-        } else {
-            space.set(space.get());
         }
-    };
+    }
     let removed = remove.len();
     for k in remove.into_iter() {
         spaces.remove(&k);
@@ -92,7 +87,8 @@ pub fn part_1(input: &Warehouse) -> Option<usize> {
             } else {
                 None
             }
-        }).count();
+        })
+        .count();
     Some(count)
 }
 
@@ -104,7 +100,7 @@ pub fn part_2(input: &Warehouse) -> Option<usize> {
     while removed > 0 {
         removed = remove_rolls(&mut input);
         total_removed += removed;
-    };
+    }
     Some(total_removed)
 }
 
