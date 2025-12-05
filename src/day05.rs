119d4b2ff83c5710e5efa9e9b7e32cf642bba3e6
Victor W Allen <vwallen@antlab.net>
2025-12-05 09:10:38 -0800

    Day 5, part 2

--- src/day05.rs
+++ src/day05.rs
@@ -1,26 +1,49 @@
 use crate::read_input_lines;
 use anyhow::Result;
-// use itertools::Itertools;
+use itertools::Itertools;
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub enum RangeBound {
+    Start,
+    End
+}
+use RangeBound::*;
 
 pub fn prepare(file_name: &str) -> Result<(Vec<(u64, u64)>, Vec<u64>)> {
     let input = read_input_lines(file_name);
-    let mut ranges:Vec<(u64, u64)> = Vec::new();
+    let mut range_bounds:Vec<(u64, RangeBound)> = Vec::new();
     let mut ids:Vec<u64> = Vec::new();
     for line in input.into_iter() {
         if let Ok((start, end)) = sscanf!(line, "{u64}-{u64}") {
-            ranges.push((start, end));
+            range_bounds.push((start, Start));
+            range_bounds.push((end, End));
         } else {
             if let Ok(id) = sscanf!(line, "{u64}") {
                 ids.push(id);
             }
         }
     }
+
+    let mut ranges:Vec<(u64, u64)> = Vec::new();
+    let mut start_stack:Vec<(u64, RangeBound)> = Vec::new();
+    for bound in range_bounds.into_iter().sorted() {
+        match bound {
+            (_, Start) => start_stack.push(bound),
+            (_, End) => {
+                if let Some(start) = start_stack.pop() {
+                    if start_stack.is_empty() {
+                        ranges.push((start.0, bound.0))
+                    }
+                }
+            },
+        }
+    }
     ranges.sort();
-    // println!("{:?}", ranges);
-    // println!("{:?}", ids);
+
     Ok((ranges, ids))
 }
 
+// Answer: 862
 pub fn part_1(input: &(Vec<(u64, u64)>, Vec<u64>)) -> Option<usize> {
     let (ranges, ids) = input;
     let count:usize = ids
@@ -36,8 +59,15 @@ pub fn part_1(input: &(Vec<(u64, u64)>, Vec<u64>)) -> Option<usize> {
     Some(count)
 }
 
-pub fn part_2(_input: &(Vec<(u64, u64)>, Vec<u64>)) -> Option<usize> {
-    None
+// Answer: 357907198933892
+pub fn part_2(input: &(Vec<(u64, u64)>, Vec<u64>)) -> Option<usize> {
+    let (ranges, _) = input;
+    let total = ranges
+        .iter()
+        .fold(0u64 , |total, (start, end)| {
+            total + end - start + 1
+        });
+    Some(total as usize)
 }
 
 #[cfg(test)]
@@ -50,7 +80,7 @@ mod test {
         if let Ok(input) = prepare("day05-example.txt") {
             let (ranges, ids) = input;
             assert_eq!(ranges.first(), Some(&(3, 5)));
-            assert_eq!(ranges.last(), Some(&(16, 20)));
+            assert_eq!(ranges.last(), Some(&(10, 20)));
             assert_eq!(ids.first(), Some(&1));
             assert_eq!(ids.last(), Some(&32));
         }
@@ -64,10 +94,9 @@ mod test {
     }
 
     #[test]
-    #[ignore]
     fn test_part_2() {
         if let Ok(input) = prepare("day05-example.txt") {
-            assert_eq!(part_2(&input), Some(1))
+            assert_eq!(part_2(&input), Some(14))
         }
     }
 }
