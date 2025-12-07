93c0e50502272c6afd863d59b75af6f2cc9582e9
Victor W Allen <vwallen@antlab.net>
2025-12-07 07:41:51 -0800

    day 7, part 1

--- src/day07.rs
+++ src/day07.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use crate::read_input_lines;
 use anyhow::Result;
 
@@ -6,8 +7,33 @@ pub fn prepare(file_name: &str) -> Result<Vec<String>> {
     Ok(input)
 }
 
-pub fn part_1(_input: &Vec<String>) -> Option<usize> {
-    None
+//Answer: 1672
+pub fn part_1(input: &Vec<String>) -> Option<usize> {
+    let mut split_count = 1;
+    let _beams:HashSet<usize> = input.into_iter()
+        .map(|line| {
+            line.chars()
+                .enumerate()
+                .filter_map(|(x, c)| if c != '.' { Some(x)} else { None })
+                .collect::<HashSet<usize>>()
+        })
+        .filter(|s| !s.is_empty())
+        .reduce(|a, b| {
+            let splits = a.intersection(&b)
+                .map(|aa| {
+                    split_count += 1;
+                    vec![aa - 1, aa + 1].into_iter()
+                })
+                .flatten()
+                .collect::<HashSet<usize>>();
+            let passthrough = a.difference(&b).map(|a| *a ).collect();
+            let beams = splits.union(&passthrough).map(|a| *a).collect::<HashSet<usize>>();
+            beams
+        }).unwrap();
+
+    split_count -= 1;
+
+    Some(split_count)
 }
 
 pub fn part_2(_input: &Vec<String>) -> Option<usize> {
@@ -20,10 +46,9 @@ mod test {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_part_1() {
         if let Ok(input) = prepare("day07-example.txt") {
-            assert_eq!(part_1(&input), Some(1))
+            assert_eq!(part_1(&input), Some(21))
         }
     }
 
